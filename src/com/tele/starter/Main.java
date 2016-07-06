135b8271bac76cd67cde729c854117e0f934d6a0
ameyxd <ameyambade@gmail.com>
2016-07-06 16:00:36 +0530

    Added Main

--- src/com/tele/starter/Main.java
+++ src/com/tele/starter/Main.java
@@ -3,7 +3,10 @@ package com.tele.starter;
 /**
  * Created with love by Amey Ambade on 6/7/16.
  */
-public class Test {
-    private String a = "";
-    private Integer a = 100;
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
 }
